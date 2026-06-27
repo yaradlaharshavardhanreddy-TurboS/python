bb6106d470a3533783c4d17cc4b80218ac31c5f1
yaradlaharshavardhanreddy-TurboS <yaradlaharshavardhanreddy@gmail.com>
2026-06-27 12:00:56 +0530

    Add files via upload
    Bitwise Operatora and its operations

---
+++ Bitwise-operators-d3.py
@@ -0,0 +1,68 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+#Bitwise OPerators
+
+a=11
+b=9
+bin(a)
+'0b1011'
+bin(b)
+'0b1001'
+a&b
+9
+# This is about &(and) operator
+
+
+#same integers for a,b
+
+#Bitwise  " | "  (OR)
+>>> a|b
+11
+>>> 
+>>> 
+>>> 
+>>> #BitWise " ~ " (Negation)
+>>> 
+>>> a=11
+>>> ~a
+-12
+>>> 
+>>> a=3
+>>> ~a
+-4
+>>> 
+>>> 
+>>> #For Negation -(a+1) is the result
+>>> 
+>>> #BitWise " ^ " (XOR)
+>>> 
+>>> 
+>>> #Same digit it is 0 that means if both are 1 or 0 it is 0 else it is 1 that is if both are opposites
+>>> 
+>>> a=11
+>>> b=9
+>>> a^b
+2
+>>> 
+>>> 
+>>> 
+>>> 
+>>> #BitWise " << "" (Lef Shift)
+>>> 
+>>> a=5
+>>> a<<2
+20
+>>> b=7
+>>> b<<2
+28
+>>> 
+>>> 
+>>> #BitWise " >> " (Right Shift)
+>>> 
+>>> a=5
+>>> a>>2
+1
+>>> a=6
+>>> a>>3
+0
