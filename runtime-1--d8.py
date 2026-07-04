0a063aa7a973f7aa7089909a654662dd09e2118e
yaradlaharshavardhanreddy-TurboS <yaradlaharshavardhanreddy@gmail.com>
2026-07-04 12:06:00 +0530

    Add files via upload

---
+++ runtime-1--d8.py
@@ -0,0 +1,15 @@
+#run_time input()
+
+a = int(input("a value"))
+
+b = int(input("b value"))
+
+option = input('''choose the option
+                                1.add
+                                2.sub
+                                3.mul''')
+
+
+print(a+b)
+print(a-b)
+print(a*b)
