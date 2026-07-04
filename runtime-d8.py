c5cbdaf337e355b032af44debb9f8cee9306b0a3
yaradlaharshavardhanreddy-TurboS <yaradlaharshavardhanreddy@gmail.com>
2026-07-04 11:44:48 +0530

    Add files via upload
    how to give input at runtime

---
+++ runtime-d8.py
@@ -0,0 +1,48 @@
+#run_time input()
+
+#int
+
+a = int(input("Enter the a value"))
+
+b = int(input("Enter the b value"))
+
+print(a+b)
+
+
+
+#float
+
+a = float(input("Enter the a value"))
+
+b = float(input("Enter the b value"))
+
+print(a+b)
+
+
+#string
+
+a = input("data1")
+
+b = input("data2")
+
+print(a+b)
+
+
+#complex
+
+a = complex(input("data1"))
+
+b = complex(input("data2"))
+
+print(a+b)
+
+
+#boolean
+
+a = bool(input("Enter the a value"))
+
+b = bool(input("Enter the b value"))
+
+print(a+b)
+
+#-------------------------------------------
