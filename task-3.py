ac7e425835e8b2ed9507925c8bb3b77f7b525c07
yaradlaharshavardhanreddy-TurboS <yaradlaharshavardhanreddy@gmail.com>
2026-07-09 12:11:43 +0530

    Add files via upload

---
+++ task-3.py
@@ -0,0 +1,183 @@
+#task 1: votinf
+
+'''age = int(input("Enter age"))
+
+if age>=18:
+          print("The Person is eligible")
+else:
+    print("The person is not eligible")'''
+
+
+#task 2: even-odd
+
+'''num = int(input("Enter a number:"))
+
+if num%2==0:
+    print(num," is even")
+else:
+    print(num," is odd")'''
+
+
+#task 3: leap year
+'''
+year = int(input("Enter Year"))
+
+if year%4==0:
+    print(year,"is a leap year")
+else:
+    print(year,"is not a leap year")'''
+
+
+#task 4: welcome-guest
+'''
+name = input("Enter name:")
+if name=="harsha":
+    print("Welcome Harsha")
+else:
+    print("Welcome",name)'''
+
+
+#task 5: welocme-guests
+'''
+name = input("enter name").lower()
+
+lst = ["harsha","sai","gopi","sumanth","prabhas"]
+
+if name in lst:
+    print("Welcome",name)
+else:
+    print("Welcom Guest")'''
+
+
+#task 6: vowels-consonants
+
+'''
+letter = input("Enter Letter").lower()
+
+lst = ['a','e','i','o','u']
+
+if letter in lst:
+    print(letter," is a vowel")
+else:
+    print(letter," is a a consonant")'''
+
+
+#task 7: username-password
+
+'''
+usernames = ["red","blue","orange","white","black"]
+passwords = ["3!","4@blue","5#or","wh12","bl21"]
+
+user = input("Enter your usename")
+
+passw = input("Enter your password")
+
+if user in usernames:
+    if passw in passwords:
+        print("Login Successful")
+    else:
+        print("Incorrect Password")
+
+else:
+    print("Invalid Username")'''
+
+
+#task 9: age-scholarship-attendance
+
+'''
+age = int(input("Enter age"))
+
+marks = int(input("Enter marks"))
+
+attendance = int(input("Enter attendance"))
+
+if age>=18:
+    print(" The person iseligible for vote")
+if marks>=80:
+    print("The person is eligible for scholarship")
+if attendance>=70:
+    print("The person is allowed for exam")
+'''
+
+#task9: cake price
+'''
+
+price =int(input("Enter your price to select cake a cake on your price:"))
+
+if price==1200:
+    print("Red Velvet Cake")
+
+elif price==1000:
+    print("Almond Cake")
+
+elif price==800:
+    print("Chocolate Cake")
+    
+elif price==600:
+    print("ButterSkoch Cake")
+
+else:
+    print("Cake is not available")'''
+
+
+#task 10: pizza
+
+item = input("Enter your item:")
+
+if item=="bbq pizza":
+    print("bbq  is 800 rupees")
+
+elif item=="crispy chicken pizza":
+    print("crispy chicken pizza is 600 rupees")
+
+elif item=="panner pizza":
+    print("paneer pizza is 400 rupees")
+
+elif item=="corn pizza":
+    print("corn pizza is 200 rupees")
+
+elif item=="frenchfries &coke":
+    print("Our combo is 150 rupees")
+
+else:
+    print("No item available")
+
+
+
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
