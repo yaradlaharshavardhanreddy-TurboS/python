ffb150874c73834dab28074eddf6cce331ea76ba
yaradlaharshavardhanreddy-TurboS <yaradlaharshavardhanreddy@gmail.com>
2026-07-16 12:50:48 +0530

    Add files via upload
    Tasks-2,3,4

---
+++ Task-5.py
@@ -0,0 +1,118 @@
+
+
+#task - splitbill
+
+''''
+def split():
+    a = int(input("Total bill"))
+    b = int(input("no of persons in group"))
+    print("share per  person is" ,a//b)
+split()
+
+'''
+
+
+#formatting method
+
+'''
+def split():
+    a = int(input("Total bill"))
+    b = int(input("no of persons in group"))
+    print(f"share per  person {a//b}")
+split()
+'''
+
+
+#dot method
+'''
+
+def split():
+    a = int(input("Total bill"))
+    b = int(input("no of persons in group"))
+    print(share per  person2
+          {a//b} :%f)
+split()
+'''
+
+
+
+
+ # task -add,sub,mul
+
+ 
+
+def cal():
+    a = int(input("Enter a"))
+             
+    b = int(input("Enter b"))
+
+    print('''Select an option 1:add
+                                2:sub
+                                3:mul''')
+
+    o = int(input("Please enter your option"))
+
+
+    if o == 1 :
+        print("the sum is: ",a+b)
+
+    elif o == 2:
+        print("the sub is: ",a-b)
+
+    elif o == 3:
+        print("the mul is: ",a*b)
+
+    else:
+        print("Please select a valid option")
+
+cal()
+
+
+
+                   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ task-4.py
@@ -0,0 +1,8 @@
+f = ["apple","banana","mango"]
+
+for i in f:
+    f1 = [f].upper()
+    print(f1)
+    
+    
+    
