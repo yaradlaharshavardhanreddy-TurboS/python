a90abf7ce8ef31fe36009a3c45add46a7b860019
yaradlaharshavardhanreddy-TurboS <yaradlaharshavardhanreddy@gmail.com>
2026-07-18 12:40:59 +0530

    Add files via upload
    unpacking *

---
+++ unpacking-d15.py
@@ -0,0 +1,98 @@
+
+
+# * arguments(* is used to unpack the elements)
+'''
+#list
+a = [2,3,4,5,6,7]
+print(a)
+print(*a)
+
+#tuple
+a = (2,3,4,5,6,7)
+print(a)
+print(*a)
+
+
+#set
+a = {2,3,4,5,6,7}
+print(a)
+print(*a)
+
+
+#dict
+
+#when we use * for dict it only returns the keys or unpacks the keys only
+
+a = {"name":"harsha","city":"kanigiri"}
+print(a)
+print(*a)
+
+
+
+a,b,c =2,3,4,5,6,7,8,9
+print(a)
+print(b)
+print(c)#error expected 3 got 8
+
+
+
+
+
+#using *
+
+*a,b,c =2,3,4,5,6,7,8,9
+print(*a)
+print(b)
+print(c)
+'''
+
+
+*a,b,c ="codegnan"
+print(*a)
+print(b)
+print(c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
