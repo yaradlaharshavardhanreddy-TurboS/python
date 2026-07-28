e21ed5ad01f6e1608675e819271d12cce4946812
yaradlaharshavardhanreddy-TurboS <yaradlaharshavardhanreddy@gmail.com>
2026-07-28 12:43:53 +0530

    Add files via upload
    mymodule,data

---
+++ data1-d21.py
@@ -0,0 +1,16 @@
+'''
+import mymodule1
+mymodule1.greetings("harsha reddy")
+
+'''
+
+
+#import mymodule1
+
+import mymodule1
+b = mymodule1.details["idnos"]
+c = mymodule1.details["names"]
+print(b)
+print(c)
+
+
---
+++ mymodule1-d21.py
@@ -0,0 +1,71 @@
+'''
+def greetings(name):
+    print("welcome",name)
+
+'''
+
+'''
+a = 4
+b = 8
+print(a+b)
+
+
+
+a = int(input("a value"))
+
+b = int(input("b value"))
+
+print(a+b)
+
+'''
+
+'''
+details = {"idnos": [10,20,30],"names":["harsha","vardhan","reddy"],"marks":[60,70,80]}
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
